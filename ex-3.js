be88f3007952a70668e4f748318552ed379d6106
TheYami <luffy90san@gmail.com>
2024-10-03 13:55:00 +0700

    feat/ex3

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,4 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+console.log(lightBulbStatus === "On" || lightBulbStatus === "Off"? `Light bulb is ${lightBulbStatus}`: "Please choose the correct input (On/Off)");
